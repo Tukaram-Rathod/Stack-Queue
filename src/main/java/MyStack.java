79f3dca82f44717cbdf2cd20373b4dda29a29939
Tukaram-Rathod <tukaram.p.rathod02@gmail.com>
2021-05-02 12:34:49 +0530

    Added.Created Stack and pushed elements into the stack

---
+++ src/main/java/MyStack.java
@@ -0,0 +1,19 @@
+    public class MyStack {
+        private final MyLinkedList myLinkedList;
+
+        public MyStack() {
+            this.myLinkedList = new MyLinkedList();
+        }
+
+        public void push(INode myNode) {
+            myLinkedList.add(myNode);
+        }
+
+        public INode peak() {
+            return myLinkedList.head;
+        }
+
+        public void printStack() {
+            myLinkedList.printmyNodes();
+        }
+    }
